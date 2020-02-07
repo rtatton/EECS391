48528fb0acba68043e59c7e8a4338dc7aed14397
rdtatton <rdt46@case.edu>
2020-02-06 23:44:49 -0500

    Major redesign implementing ActionPlan.
    Signed-off-by: rdtatton <rdt46@case.edu>

--- src/edu/cwru/sepia/agent/MyResourceAgent.java
+++ src/edu/cwru/sepia/agent/MyResourceAgent.java
@@ -4,15 +4,13 @@ import edu.cwru.sepia.action.Action;
 import edu.cwru.sepia.environment.model.history.History.HistoryView;
 import edu.cwru.sepia.environment.model.state.State.StateView;
 import edu.cwru.sepia.environment.model.state.Unit.UnitView;
-import utils.ActionsMap;
+import utils.ActionPlan;
 import utils.PlayerView;
-import utils.PlayerView.Units;
 
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 
 public class MyResourceAgent extends Agent
@@ -22,47 +20,39 @@ public class MyResourceAgent extends Agent
     public MyResourceAgent(int playerNum)
     {
         super(playerNum);
-        this.player = new PlayerView(playerNum);
-        System.out.println("Time to build!");
+        this.player = PlayerView.createPlayer(playerNum);
     }
 
     @Override
     public Map<Integer, Action> initialStep(StateView state, HistoryView history)
     {
-        getPlayer().setState(state);
-        getPlayer().setHistory(history);
         return middleStep(state, history);
     }
 
     @Override
     public Map<Integer, Action> middleStep(StateView state, HistoryView history)
     {
-        ActionsMap actionsMap = new ActionsMap(getPlayerNumber());
-        UnitView townHall = getPlayer().getUnitsByType(Units.TOWNHALL).get(0);
+        getPlayer().setStateAndHistory(state, history);
 
-        // TODO "ActionTree" that partitions remaining, unassigned agents to another Action.
-        // TODO Wrapper class: ActionPlan? Allow task ordering, priority, and capping number of times task is done.
+        List<UnitView> availableUnits = getPlayer().getUnoccupiedUnits();
+        ActionPlan actionPlan = ActionPlan.createActionPlan();
 
-        Map<Boolean, List<UnitView>> cargoStatus = getPlayer().getUnoccupiedUnits()
-                .stream()
-                .collect(Collectors.partitioningBy(getPlayer()::isCarryingCargo));
-        List<UnitView> remaining = cargoStatus.get(false);
+        actionPlan.scheduleAscendingSize(
+                getPlayer().buildFarm(availableUnits),
+                getPlayer().buildPeasant(availableUnits),
+                getPlayer().buildFootman(availableUnits),
+                getPlayer().gatherBalancedResources(availableUnits),
+                getPlayer().depositResources(availableUnits)
+        );
+        actionPlan.addUnitsToPool(availableUnits);
+        actionPlan.createPlan();
 
-        ActionsMap depositResources = getPlayer().depositResources(cargoStatus.get(true), townHall);
-        UnitView farmBuilder = remaining.remove(remaining.size() - 1);
-        ActionsMap buildFarm = getPlayer().buildFarm(farmBuilder);
-        ActionsMap buildPeasant = getPlayer().buildPeasant(townHall);
-        ActionsMap gatherResources = getPlayer().gatherBalancedResources(remaining, townHall);
-
-        actionsMap.assign(buildPeasant, depositResources, buildFarm, gatherResources);
-
-        return actionsMap.getMap();
+        return actionPlan.getActionPlanMap();
     }
 
     @Override
     public void terminalStep(StateView state, HistoryView history)
     {
-        System.out.println("All in a day's work!");
     }
 
     @Override
