5a95a7102488ce08b124556106b264cdf15d35df
rdtatton <rdt46@case.edu>
2020-02-06 23:11:34 -0500

    Renamed ActionsMap to ActionMap and ActionMap.isEffective.
    ActionMap.isEffective checks if the ActionMap contains an Actions.
    
    Signed-off-by: rdtatton <rdt46@case.edu>

---
+++ src/utils/ActionMap.java
@@ -0,0 +1,100 @@
+package utils;
+
+import edu.cwru.sepia.action.Action;
+
+import java.util.*;
+
+/**
+ * Wrapper to store instances of SEPIA Actions. An ActionMap maintains the {@code HashMap<Integer, Action>} underlying
+ * data structure that SEPIA uses to access Actions. The advantages of an ActionMap include being able to group
+ * multiple instances of the same type of Action to multiple units and creating/assigning Actions in a more
+ * syntactically
+ * understandable manner (i.e., with the {@link #assign(Action...)} or {@link #assign(ActionMap...)} methods).
+ *
+ * @author Ryan Tatton
+ */
+public class ActionMap
+{
+    private HashMap<Integer, Action> actionMap;
+
+    public ActionMap(ActionMap map)
+    {
+        this.actionMap = new HashMap<>(map.getMap());
+    }
+
+    public ActionMap()
+    {
+        this.actionMap = new HashMap<>();
+    }
+
+    /**
+     * Factory method for instantiating an empty ActionMap.
+     *
+     * @return A new, empty ActionMap.
+     */
+    public static ActionMap createActionsMap()
+    {
+        return new ActionMap();
+    }
+
+    /**
+     * Safely copies the contents of one ActionMap into a new ActionMap. Used in the sorting methods defined in
+     * {@link ActionGroup#sortAscending()}, and {@link ActionGroup#sortDescending()}.
+     *
+     * @return New copy of an ActionMap, with its contents.
+     */
+    public ActionMap copy()
+    {
+        return new ActionMap(this);
+    }
+
+    /**
+     * Adds one or more {@link Action}s, to {@link #actionMap}.
+     *
+     * @param actions One or more {@link Action}s to assign to the unit that is specified.
+     */
+    public void assign(Action... actions)
+    {
+        Arrays.stream(actions).forEach(action -> getMap().put(action.getUnitId(), action));
+    }
+
+    /**
+     * Adds all {@link Action}s of one or more {@link ActionMap}s to this instance's {@link #actionMap}.
+     *
+     * @param newActions One or more {{@link #actionMap}}s to add.
+     */
+    public void assign(ActionMap... newActions)
+    {
+        Arrays.stream(newActions).forEach(actions -> getMap().putAll(actions.getMap()));
+    }
+
+    public int size()
+    {
+        return getMap().size();
+    }
+
+    public boolean isEffective()
+    {
+        return getMap().size() > 0;
+    }
+
+    public Collection<Action> getActionMap()
+    {
+        return getMap().values();
+    }
+
+    public List<Integer> getAssignedUnitIds()
+    {
+        return new ArrayList<>(getMap().keySet());
+    }
+
+    public HashMap<Integer, Action> getMap()
+    {
+        return actionMap;
+    }
+
+    public String toString()
+    {
+        return "ActionsMap{" + "size=" + getMap().size() + "}";
+    }
+}
